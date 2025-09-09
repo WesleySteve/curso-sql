c129ea9b1614177addd28d612a4ebca594106228
Wesley Steve <wesley.silva23@hotmail.com>
2025-09-09 11:55:27 -0300

    src/sql/querys_curso/basico/exercicios/tamanho_desc_maior_que_cem.sql

---
+++ src/sql/querys_curso/basico/exercicios/tamanho_desc_maior_que_cem.sql
@@ -0,0 +1,11 @@
+SELECT
+  product_category_name,
+  AVG(product_description_lenght) AS tamanho_medio_desc,
+  MAX(product_description_lenght) AS tamanho_max_desc,
+  MIN(product_description_lenght) AS tamanho_min_desc
+FROM
+  olist_products
+WHERE
+  product_description_lenght >= 100
+GROUP BY
+  1
