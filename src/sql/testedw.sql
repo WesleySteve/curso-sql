4df4496eb80df9c7b885ee9f4ba852d3829ae37e
Wesley Steve <wesley.silva23@hotmail.com>
2025-09-24 11:43:08 -0300

    src/sql/querys_comparativo_curso/intermediario/intermediario17.sql query no dw de comparativo a query no oltp

--- src/sql/testedw.sql
+++ src/sql/testedw.sql
@@ -1,4 +1,14 @@
 SELECT
-  *
+  seller_id,
+  strftime ('%Y-%m-%d', order_approved_at) AS data_venda,
+  SUM(price) AS valor_vendas
 FROM
   fato_orders
+WHERE
+  seller_id IS NOT NULL
+GROUP BY
+  1,
+  2
+ORDER BY
+  1,
+  2 ASC
