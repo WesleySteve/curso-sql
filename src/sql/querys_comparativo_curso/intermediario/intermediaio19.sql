d470013e3451825d13f34f1d33d81b77f5866abb
Wesley Steve <wesley.silva23@hotmail.com>
2025-09-24 11:51:31 -0300

    src/sql/querys_comparativo_curso/intermediario/intermediaio19.sql query no dw comparativo com oltp

---
+++ src/sql/querys_comparativo_curso/intermediario/intermediaio19.sql
@@ -0,0 +1,39 @@
+WITH
+  vendas AS (
+    SELECT
+      seller_id,
+      strftime ('%Y-%m-%d', order_approved_at) AS data_venda,
+      SUM(price) AS valor_vendas
+    FROM
+      fato_orders
+    WHERE
+      seller_id IS NOT NULL
+    GROUP BY
+      1,
+      2
+    ORDER BY
+      1,
+      2 ASC
+  )
+SELECT
+  *,
+  COALESCE(
+    LAG (valor_vendas) OVER (
+      PARTITION BY
+        seller_id
+      ORDER BY
+        data_venda ASC
+    ),
+    0
+  ) AS anterior,
+  COALESCE(
+    LEAD (valor_vendas) OVER (
+      PARTITION BY
+        seller_id
+      ORDER BY
+        data_venda ASC
+    ),
+    0
+  ) AS posterior
+FROM
+  vendas
--- src/sql/testedw.sql
+++ src/sql/testedw.sql
@@ -1,14 +1,4 @@
 SELECT
-  seller_id,
-  strftime ('%Y-%m-%d', order_approved_at) AS data_venda,
-  SUM(price) AS valor_vendas
+  *
 FROM
   fato_orders
-WHERE
-  seller_id IS NOT NULL
-GROUP BY
-  1,
-  2
-ORDER BY
-  1,
-  2 ASC
